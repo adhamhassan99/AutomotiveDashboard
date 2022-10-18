f60b51d6c09ef7cb2e676a35bc536a4e4a1aeaf2
Adham Hassan <49523561+adhamhassan99@users.noreply.github.com>
2022-10-18 17:35:00 +0200

    Added Send msg on Enter in Messaging

--- auto-dash/src/components/ActiveChatFooter/ActiveChatFooter.component.jsx
+++ auto-dash/src/components/ActiveChatFooter/ActiveChatFooter.component.jsx
@@ -4,11 +4,15 @@ import { ReactComponent as AttachIcon } from "../../Assets/AttachIcon.svg";
 import { ReactComponent as SendIcon } from "../../Assets/SendIcon.svg";
 
 const ActiveChatFooter = ({ onChange, onClick, value }) => {
+  const keydown = (e) => {
+    if (e.key === "Enter") onClick();
+  };
   return (
     <div className="active-chat-footer">
       <div className="left-col">
         <AttachIcon />
         <input
+          onKeyDown={keydown}
           value={value}
           onChange={onChange}
           type="text"
